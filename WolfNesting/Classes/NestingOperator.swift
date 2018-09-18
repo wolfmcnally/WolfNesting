c487c84d478bdfbd74e99c4b0e6c182a4fb0e82f
Wolf McNally <wolf@wolfmcnally.com>
2018-09-18 12:37:59 -0700

    Cleanup. Updated pod version.

--- WolfNesting/Classes/NestingOperator.swift
+++ WolfNesting/Classes/NestingOperator.swift
@@ -3,7 +3,6 @@
 //  WolfNesting
 //
 //  Created by Wolf McNally on 09/05/2018.
-//  Copyright © 2018 Wolf McNally.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
